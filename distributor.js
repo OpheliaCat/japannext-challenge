bd267b1e7b342beb23a3da1af096118f0f549002
Ophelia <ophelia.pavlova@gmail.com>
2024-04-15 06:39:29 -0700

    added unsub & unadv, improved error handling

--- distributor.js
+++ distributor.js
@@ -22,75 +22,112 @@ const saveSub = (topic, key, ws) => {
     const endpoint = `${topic}/${key}`;
     subs[endpoint] = subs[endpoint] ? subs[endpoint].add(ws) : new Set([ws]);
     console.log('%s has subscribed to %s: ', ws._socket.remoteAddress, endpoint);
+    return endpoint;
 };
 
-const saveAd = (topicRegex, ws) => {
-    const ip = ws._socket.remoteAddress;
+const deleteSub = (topic, key, ws) => {
+    const endpoint = `${topic}/${key}`;
+    if (!subs[endpoint] || !subs[endpoint].delete(ws)) {
+        throw new Error(`err:you_have_not_subscribed_to_${endpoint}_yet`);
+    }
+    console.log('%s has unsubscribed from %s: ', ws._socket.remoteAddress, endpoint);
+    return endpoint;
+};
+
+const saveAd = (topicRegex, ip) => {
     ads[ip] = ads[ip] ? ads[ip].add(topicRegex) : new Set([topicRegex]);
     console.log('%s has advertised for %s: ', ip, topicRegex);
 };
 
-const writeValue = (topic, key, value) => {
-    const ip = ws._socket.remoteAddress;
+const deleteAd = (topicRegex, ip) => {
+    if(!ads[ip] || !ads[ip].delete(topicRegex)) {
+        throw new Error(`err:you_have_not_advertised_for_${topicRegex}_yet`);
+    }
+    console.log('%s has advertised for %s: ', ip, topicRegex);
+};
+
+const writeValue = (topic, key, value, ip) => {
     endpoint = `${topic}/${key}`;
     if (!ads[ip] || !ads[ip].reduce((acc, regex) => acc || new RegExp(regex).test(endpoint), false)){
-        return false;
+        throw new Error(`err:you_are_not_a_valid_publisher_for_${endpoint}`);
     };
-    console.log('%s has published new data for %s: ', ip, endpoint);
     let ref = resources;
     topic.split('/').forEach(segment => {
+        if (!ref[segment]) ref[segment] = {};
         ref = ref[segment];
     });
+    if (!ref[key]) throw new Error(`err:${key}_is_not_a_valid_key`);
     ref[key] = value;
-    return true;
+    console.log('%s has published new data for %s: ', ip, endpoint);
+    console.log('Resources: ', JSON.stringify(resources, null, 2));
+    return `${endpoint}=${value}`;
 };
 
 // Create a WebSocket server
 const wss = new WebSocket.Server({ port: 8080 });
 
 // Event listener for new connections
-wss.on('connection', ws => {
+wss.on('connection', (ws, req) => {
+    const ip = req.connection.remoteAddress;
+    console.log('Lets welcome our new guest: ', ip);
     // Event listener for incoming messages
     // message examples: 
     // adv:market-data/TSE/.*  
     // sub:market-data/TSE/Sony:ltp,ltq,open,high,low
     // pub:market-data/TSE/Sony:ltp=13335,ltq=500,open=13475,high=13535,low=13290
-    ws.on('message', ({ data, origin }) => {  
-        console.log('Received message:', data, 'from', origin);
+    ws.on('message', ({ data }) => {  
+        console.log('Received message:', data);
         const [command, topic, dataPairs] = String(data).trim().split(':');
-        let endpoint = '';
-    
-        switch (command) {
-            case 'adv':
-                // advertise
-                saveAd(topic, ws);
-                break;
-            case 'sub':
-                // subscribe
-                dataPairs.split(',').forEach(pair => {
-                    saveSub(topic, pair.split('=')[0], ws);
-                });
-                break;
-            case 'pub':
-                // publish
-                dataPairs.split(',').forEach(pair => {
-                    const [key, value] = pair.split('='); // [ltp, 13335]
-                    if (!writeValue(topic, key, value)) {
-                        ws.send(`err: not a valid publisher for ${endpoint}`);
-                        return;
-                    };
-                    if (subs.has(endpoint)) {
-                        subs[endpoint].forEach(client => {
-                            if (client.readyState === WebSocket.OPEN) client.send(`pub:${topic}:${pair}`);
-                        });
-                    }
-                });
-                break;
-            default:
-                console.log('Unknown command:', command);
+        const responseData = [];
+
+        try {
+            switch (command) {
+                case 'adv':
+                    // advertise
+                    saveAd(topic, ip);
+                    ws.send(`adv:${topic}`);
+                    break;
+                case 'sub':
+                    // subscribe
+                    dataPairs.split(',').forEach(key => {
+                        responseData.push(saveSub(topic, key, ws));
+                    });
+                    ws.send(`sub:${responseData.join(',')}`);
+                    break;
+                case 'pub':
+                    // publish
+                    dataPairs.split(',').forEach(pair => {
+                        const [key, value] = pair.split('='); // [ltp, 13335]
+                        responseData.push(writeValue(topic, key, value, ip));
+                        if (subs.has(endpoint)) {
+                            subs[endpoint].forEach(client => {
+                                if (client.readyState === WebSocket.OPEN) client.send(`pub:${topic}:${pair}`);
+                            });
+                        }
+                    });
+                    ws.send(`pub:${responseData.join(',')}`);
+                    break;
+                case 'unadv':
+                    // unadvertise
+                    deleteAd(topic, ip);
+                    ws.send(`unadv:${topic}`);
+                    break;
+                case 'unsub':
+                    // unsubscribe
+                    dataPairs.split(',').forEach(key => {
+                        responseData.push(deleteSub(topic, key, ws));
+                    });
+                    ws.send(`unsub:${responseData.join(',')}`);
+                    break;
+                default:
+                    console.log('Unknown command: ', command);
+                    ws.send('err:unknown_command:supported_commands=adv,sub,pub,unadv,unsub');
+            }
+            // Send a response back to the client
+            ws.send('Incorrect message format. Please use adv:topic, sub:topic, or pub:topic:data');
+        } catch (error) {
+            ws.send(`err:${error.message}`);
         }
-        // Send a response back to the client
-        ws.send('Incorrect message format. Please use adv:topic, sub:topic, or pub:topic:data');
     });
     
     // Event listener for connection close
@@ -98,14 +135,5 @@ wss.on('connection', ws => {
         console.log('Client disconnected');
     });
 });
-
-const interval = setInterval(function ping() {
-    wss.clients.forEach(ws => {
-        if (ws.isAlive === false) return ws.terminate();
-
-        ws.isAlive = false;
-        ws.ping();
-    });
-}, 20000);
 
 console.log('WebSocket server is running on port 8080');
